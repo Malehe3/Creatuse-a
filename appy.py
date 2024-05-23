e3e4216c28f5c671675f0f748e4eeed48a877ec8
Malehe3 <160879306+Malehe3@users.noreply.github.com>
2024-05-22 19:07:50 -0500

    Update appy.py

--- appy.py
+++ appy.py
@@ -1,7 +1,11 @@
-Import streamlit as st
+import os
+import streamlit as st
 from PIL import Image, ImageDraw, ImageFont
+from bokeh.models.widgets import Button
+from bokeh.models import CustomJS
+from streamlit_bokeh_events import streamlit_bokeh_events
 
-st.title(" ¡Aprende Lenguaje de Señas Colombiano!")
+st.title("¡Aprende Lenguaje de Señas Colombiano!")
 
 st.write("""
 ### Básico: Tu Señal de Identificación
@@ -24,7 +28,42 @@ st.write("""
 Captura una característica distintiva, ya sea física, de personalidad o relacionada con una experiencia memorable y crea tu propia seña:
 """)
 
-img_file_buffer = st.camera_input("Toma una Foto")
+# Botón para activar el reconocimiento de voz
+stt_button = Button(label="Comienza", width=200, button_type="success")
+
+stt_button.js_on_event("button_click", CustomJS(code="""
+    var recognition = new webkitSpeechRecognition();
+    recognition.continuous = true;
+    recognition.interimResults = true;
+
+    recognition.onresult = function (e) {
+        var value = "";
+        for (var i = e.resultIndex; i < e.results.length; ++i) {
+            if (e.results[i].isFinal) {
+                value += e.results[i][0].transcript;
+            }
+        }
+        if (value.toLowerCase().includes("foto")) {
+            document.dispatchEvent(new CustomEvent("GET_TEXT", {detail: value}));
+        }
+    }
+    recognition.start();
+    """))
+
+result = streamlit_bokeh_events(
+    stt_button,
+    events="GET_TEXT",
+    key="listen",
+    refresh_on_update=False,
+    override_height=75,
+    debounce_time=0)
+
+img_file_buffer = None
+
+# Si se detecta la palabra "Foto" mediante el reconocimiento de voz
+if result and "GET_TEXT" in result:
+    st.write("Palabra 'Foto' detectada, tomando foto...")
+    img_file_buffer = st.camera_input("Toma una Foto")
 
 if img_file_buffer is not None:
     image = Image.open(img_file_buffer)
@@ -36,7 +75,9 @@ if img_file_buffer is not None:
         file_name="señal_identificacion.jpg",
          mime="image/jpeg" 
     )
+
 st.write("""
 ### ¡Comparte tu Señal!
 Una vez que hayas creado tu señal de identificación, compártela con tus amigos y familiares para que puedan reconocerte fácilmente en la comunidad.
 """)
+
